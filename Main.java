bfb09a3c3b49d754b34d072b6bc1672a9e66047f
Pieter <censornv@gmail.com>
2018-05-31 15:05:33 +0200

    maak array van resultaat dobbelen

--- Main.java
+++ Main.java
@@ -3,54 +3,48 @@ package com.company;
 import java.util.Random;
 import java.util.Scanner;
 
-class Main{
+class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Yahtzee!");
 
         Dobbelsteen dobbelsteen = new Dobbelsteen();
+        int[] resultatenDobbelen = new int[5];
 
         boolean doorspelen = true;
 
-        while(doorspelen = true){
-            String input = toonMenu();
-            switch(input){
+        while (doorspelen == true) {
+
+            String input = toonDobbelMenu();
+            switch (input) {
+
                 case "d":
 
-                    int dobbelResultaat1 = dobbelsteen.dobbelen();
-                    System.out.println("dobbelsteen 1: " + dobbelResultaat1);
-
-                    int dobbelResultaat2 = dobbelsteen.dobbelen();
-                    System.out.println("dobbelsteen 2: " + dobbelResultaat2);
-
-                    int dobbelResultaat3 = dobbelsteen.dobbelen();
-                    System.out.println("dobbelsteen 3: " + dobbelResultaat3);
-
-                    int dobbelResultaat4 = dobbelsteen.dobbelen();
-                    System.out.println("dobbelsteen 4: " + dobbelResultaat4);
-
-                    int dobbelResultaat5 = dobbelsteen.dobbelen();
-                    System.out.println("dobbelsteen 5: " + dobbelResultaat5);
+                    for(int x = 0; x < 5; x++){
+                        resultatenDobbelen[x] = dobbelsteen.dobbelen();
+                        System.out.println("dobbelsteen " + (x + 1) + " = " + resultatenDobbelen[x]);
+                    }
 
                     break;
+
                 case "s":
                     doorspelen = false;
             }
 
         } //while loop eindigt hier
 
-        System.out.println("Bedankt voor het spelen");
+        System.out.println("Bedankt voor het spelen!");
 
     }
 
-    static String toonMenu() {
+    static String toonDobbelMenu() {
         System.out.println("Voer uw input in: d om te dobbelen of s om te stoppen");
         Scanner scanner = new Scanner(System.in);
         return scanner.next();
     }
-}
 
+}
 class Dobbelsteen{
 
     int dobbelen(){
@@ -62,5 +56,4 @@ class Dobbelsteen{
         //let op: waarden die terug kunnen komen liggen tussen 0 - 5 (inclusief)!
         return (resultaat + 1);
     }
-
 }
