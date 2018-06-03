6f48368f1a984f12b2a3cd1f64e33eeb67b3d3a5
Pieter <censornv@gmail.com>
2018-06-03 12:32:52 +0200

    code clean-up

--- Yahtzee.java
+++ Yahtzee.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Yahtzee {
 
-    Speler speler1      = new Speler("speler1");
-    Scanner input       = new Scanner( System.in );
-    boolean doorspelen  = true;
+    Speler speler1 = new Speler("speler1");
+    Scanner input = new Scanner(System.in);
+    boolean doorspelen = true;
 
 
     public void beginSpel() {
@@ -18,11 +18,17 @@ public class Yahtzee {
         System.out.println("==============================");
         System.out.println("1: Start eerste gooi | 2: Stop spel");
 
-        boolean keuzeGemaakt = false;
-
 
         //begin menu
-        while(!keuzeGemaakt) {
+        beginMenu();
+        //spelen
+        spelSpelen();
+
+    }
+
+    void beginMenu() {
+        boolean keuzeGemaakt = false;
+        while (!keuzeGemaakt) {
             int keuze = input.nextInt();
             //dobbelen
             if (keuze == 1) {
@@ -30,7 +36,7 @@ public class Yahtzee {
                 System.out.println();
                 speler1.dobbelKeer++;
                 keuzeGemaakt = true;
-            //stoppen
+                //stoppen
             } else if (keuze == 2) {
                 doorspelen = false;
                 keuzeGemaakt = true;
@@ -39,90 +45,102 @@ public class Yahtzee {
                 continue;
             }
         }
+    }
 
-        //speler kiest welke dobbelstenen vastgelegd worden
+    void spelSpelen() {
+
         while (doorspelen) {
-            if(speler1.dobbelKeer < 4){ //redundant?
-                speler1.resetVastgelegdeResultaten(); //reset vastgelegde dobbelstenen als die er zijn
-                for (int x = 0; x < 5; x++) {
-                    boolean vastleggenKeuzeGemaakt = false;
-                    System.out.println("Dobbelsteen " + (x + 1) + " met waarde: " + speler1.resultatenDobbelen[x] + " vastleggen? 1: ja | 2: nee");
-                    while (!vastleggenKeuzeGemaakt) {
-                        int keuze = input.nextInt();
-                        if (keuze == 1) {
-                            speler1.vastgelegdeResultaten[x] = speler1.resultatenDobbelen[x];
-                            speler1.printResultatenDobbelen();
-                            System.out.println("Dobbelsteen " + (x + 1) + " met waarde: " + speler1.resultatenDobbelen[x] + " vastgelegd");
-                            speler1.printVastgelegdeResultaten();
-                            vastleggenKeuzeGemaakt = true;
-                        } else if (keuze == 2) {
-                            speler1.printResultatenDobbelen();
-                            System.out.println("Dobbelsteen " + (x + 1) + " met waarde: " + speler1.resultatenDobbelen[x] + " NIET vastgelegd");
-                            speler1.printVastgelegdeResultaten();
-                            vastleggenKeuzeGemaakt = true;
-                        } else {
-                            System.out.println("Ongeldige waarde! 1: vastleggen | 2: nee");
-                            continue;
-                        }
-                    }
-                }
-                //volgende keer gooien deze beurt - max 3 per beurt
-                speler1.dobbelKeer++;
-                //beurt voorbij?
-
-                System.out.println("==============================");
-                System.out.println(speler1.dobbelKeer + "e keer dobbelen voor beurt " + speler1.beurt);
-                System.out.println("==============================");
-                speler1.printVastgelegdeResultaten();
-            }
+            //speler kiest welke dobbelstenen vastgelegd worden
+            vastLeggenDobbelstenen();
 
             //vragen door te gaan naar volgende keer dobbelen in beurt
-            if(speler1.dobbelKeer < 3){
-                System.out.println("1: Verder dobbelen met niet-vastgelegde dobbelstenen | 2: spel stoppen");
-                boolean volgendeWorp = false;
-                while(!volgendeWorp) {
-                    int keuze = input.nextInt();
-                    if (keuze == 1) {
-                        speler1.dobbelenNaEersteKeer();
-                        volgendeWorp = true;
-                    } else if (keuze == 2) {
-                        doorspelen = false;
-                        volgendeWorp = true;
-                    } else {
-                        System.out.println("Ongeldige waarde! 1: verder dobbelen | 2: spel stoppen");
-                        continue;
-                    }
-                }
-            }
+            vraagDoorDobbelen();
 
             //start nieuwe beurt na 3 keer dobbelen
-            if(speler1.dobbelKeer == 3){
-                System.out.println("Waarden dobbelstenen beurt " + speler1.beurt + ":");
-                speler1.dobbelenNaEersteKeer();
-                System.out.println("Einde beurt " + speler1.beurt + "! 1: nieuwe beurt starten | 2: spel stoppen");
-                speler1.dobbelKeer = 1;
-                boolean nieuweBeurt = false;
-                while(!nieuweBeurt){
-                    int keuze = input.nextInt();
-                    if(keuze == 1){
-                        speler1.beurt++;
-                        System.out.println("==============================");
-                        System.out.println("1e keer dobbelen voor beurt " + speler1.beurt);
-                        System.out.println("==============================");
-                        speler1.dobbelenEersteKeer();
-                        System.out.println();
-                        speler1.dobbelKeer = 1;
-                        nieuweBeurt = true;
-                    } else if(keuze == 2){
-                        doorspelen = false;
-                        nieuweBeurt = true;
-                    } else {
-                        System.out.println("Ongeldige waarde! 1: nieuwe beurt | 2: spel stoppen");
-                        continue;
-                    }
+            startNieuweBeurt();
+        }
+        System.out.println("Programma stopt");
+    }
+
+    void vastLeggenDobbelstenen() {
+        speler1.resetVastgelegdeResultaten(); //reset vastgelegde dobbelstenen als die er zijn
+        for (int x = 0; x < 5; x++) {
+            boolean vastleggenKeuzeGemaakt = false;
+            System.out.println("Dobbelsteen " + (x + 1) + " met waarde: " + speler1.resultatenDobbelen[x] + " vastleggen? 1: ja | 2: nee");
+            while (!vastleggenKeuzeGemaakt) {
+                int keuze = input.nextInt();
+                if (keuze == 1) {
+                    speler1.vastgelegdeResultaten[x] = speler1.resultatenDobbelen[x];
+                    speler1.printResultatenDobbelen();
+                    System.out.println("Dobbelsteen " + (x + 1) + " met waarde: " + speler1.resultatenDobbelen[x] + " vastgelegd");
+                    speler1.printVastgelegdeResultaten();
+                    vastleggenKeuzeGemaakt = true;
+                } else if (keuze == 2) {
+                    speler1.printResultatenDobbelen();
+                    System.out.println("Dobbelsteen " + (x + 1) + " met waarde: " + speler1.resultatenDobbelen[x] + " NIET vastgelegd");
+                    speler1.printVastgelegdeResultaten();
+                    vastleggenKeuzeGemaakt = true;
+                } else {
+                    System.out.println("Ongeldige waarde! 1: vastleggen | 2: nee");
+                    continue;
                 }
             }
         }
-        System.out.println("Programma stopt");
+        //volgende keer gooien deze beurt - max 3 per beurt
+        speler1.dobbelKeer++;
+
+        System.out.println("==============================");
+        System.out.println(speler1.dobbelKeer + "e keer dobbelen voor beurt " + speler1.beurt);
+        System.out.println("==============================");
+        speler1.printVastgelegdeResultaten();
+    }
+
+    void vraagDoorDobbelen(){
+        if (speler1.dobbelKeer < 3) {
+            System.out.println("1: Verder dobbelen met niet-vastgelegde dobbelstenen | 2: spel stoppen");
+            boolean volgendeWorp = false;
+            while (!volgendeWorp) {
+                int keuze = input.nextInt();
+                if (keuze == 1) {
+                    speler1.dobbelenNaEersteKeer();
+                    volgendeWorp = true;
+                } else if (keuze == 2) {
+                    doorspelen = false;
+                    volgendeWorp = true;
+                } else {
+                    System.out.println("Ongeldige waarde! 1: verder dobbelen | 2: spel stoppen");
+                    continue;
+                }
+            }
+        }
+    }
+
+    void startNieuweBeurt(){
+        if (speler1.dobbelKeer == 3) {
+            System.out.println("Waarden dobbelstenen beurt " + speler1.beurt + ":");
+            speler1.dobbelenNaEersteKeer();
+            System.out.println("Einde beurt " + speler1.beurt + "! 1: nieuwe beurt starten | 2: spel stoppen");
+            speler1.dobbelKeer = 1;
+            boolean nieuweBeurt = false;
+            while (!nieuweBeurt) {
+                int keuze = input.nextInt();
+                if (keuze == 1) {
+                    speler1.beurt++;
+                    System.out.println("==============================");
+                    System.out.println("1e keer dobbelen voor beurt " + speler1.beurt);
+                    System.out.println("==============================");
+                    speler1.dobbelenEersteKeer();
+                    System.out.println();
+                    speler1.dobbelKeer = 1;
+                    nieuweBeurt = true;
+                } else if (keuze == 2) {
+                    doorspelen = false;
+                    nieuweBeurt = true;
+                } else {
+                    System.out.println("Ongeldige waarde! 1: nieuwe beurt | 2: spel stoppen");
+                    continue;
+                }
+            }
+        }
     }
 }
